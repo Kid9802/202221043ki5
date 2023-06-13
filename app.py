1a1d1dbe2e25878939824ddf35d2b5cd3554d6d3
Kid9802 <135934934+Kid9802@users.noreply.github.com>
2023-06-13 12:59:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,25 @@
-import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.signal as signal
+from scipy import signal
 
-def main():
-    st.title('전달함수 분석')
+# 전달함수 정의
+num = [100]
+den = [1, 5, 6]  # 폐루프 전달함수의 분모
 
-    # 전달 함수 G(s)
-    num = [100]
-    den = [1, 5, 6]  # (s+2)(s+3)
+# 폐루프 전달함수 계산
+G = signal.TransferFunction(num, den)
 
-    # 폐루프 전달 함수 계산
-    closed_loop_tf = signal.TransferFunction(num, den)
+# Unit step 입력에 대한 응답 곡선 계산
+t, y = signal.step(G)
 
-    # unit step 입력의 응답 계산
-    t, y = signal.step(closed_loop_tf)
+# 주파수 응답 보드선도 계산
+w, mag, phase = signal.bode(G)
 
-    # 응답 곡선 그리기
-    fig1, ax1 = plt.subplots()
-    ax1.plot(t, y)
-    ax1.set_xlabel('Time')
-    ax1.set_ylabel('Output')
-    ax1.set_title('Step Response of Closed-Loop System')
-    ax1.grid(True)
-    st.pyplot(fig1)
-
-    # 주파수 응답 그리기
-    w, mag, phase = signal.bode(closed_loop_tf)
-
-    fig2, (ax2, ax3) = plt.subplots(nrows=2)
-    ax2.semilogx(w, mag)  # 주파수 응답 그래프
-    ax2.set_xlabel('Frequency')
-    ax2.set_ylabel('Magnitude (dB)')
-    ax2.set_title('Bode Plot - Magnitude Response')
-    ax2.grid(True)
-
-    ax3.semilogx(w, phase)  # 위상 응답 그래프
-    ax3.set_xlabel('Frequency')
-    ax3.set_ylabel('Phase (degrees)')
-    ax3.set_title('Bode Plot - Phase Response')
-    ax3.grid(True)
-
-    st.pyplot(fig2)
-
-if __name__ == '__main__':
-    main()
+# 응답 곡선 그리기
+plt.figure()
+plt.plot(t, y)
+plt.xlabel('Time')
+plt.ylabel('Output')
+plt.title('Step Response')
+plt.grid(True)
+plt.show()
